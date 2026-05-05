c505d66824a8942a2854eaf1db66a320e529dcdc
callac2 <callac2@nycstudents.net>
2026-05-05 09:26:19 -0400

    Update customers.py

--- customers.py
+++ customers.py
@@ -1,2 +1,71 @@
-class costumer:
-    
+""" class costumer:
+    def __init__(self, name, order):
+        self.name = name 
+        self.order = order
+
+    customer_order = input("Please choose your order from the ") """
+class server:
+    order = input("your order to make is")
+
+    customer_orders = [
+{
+    "name": "Julia",
+    "price": 12.99,
+    "food type": "waffles with syrup and blueberries",
+    "instructions": "Toast the waffles, add syrup, add blueberries."
+},
+{
+    "name": "Blue T-shirt",
+    "price": 12.99,
+    "department": "women's shirts",
+    "description": "100 percent cotton, color: blue."
+},
+{
+    "name": "Red T-shirt",
+    "price": 12.99,
+    "department": "women's shirts",
+    "description": "100 percent cotton, color: red"
+},
+{
+    "name": "Black T-shirt",
+    "price": 12.99,
+    "department": "women's shirts",
+    "description": "100 percent cotton, color: black"
+},
+{
+    "name": "Purple T-shirt",
+    "price": 12.99,
+    "department": "women's shirts",
+    "description": "100 percent cotton, color: purple"
+},
+{
+    "name": "High Rise Jean shorts",
+    "price": 24.99,
+    "department": "women's shorts",
+    "description": "100 percent cotton denim, color: denim blue"
+},
+{
+    "name": "black sweatpants, fold-over waistband",
+    "price": 20.99,
+    "department": "women's pants",
+    "description": "100 percent cotton, color: black"
+},
+{
+    "name": "long sleeveless dress",
+    "price": 54.99,
+    "department": "women's formalwear",
+    "description": "100 percent cotton denim, color: pink"
+},
+{
+    "name": "short sleeveless dress",
+    "price": 54.99,
+    "department": "women's formalwear",
+    "description": "100 percent cotton denim, color: blue"
+},
+{
+    "name": "long sleeve dress",
+    "price": 54.99,
+    "department": "women's formalwear",
+    "description": "100 percent cotton denim, color: pink"
+},
+    ]
