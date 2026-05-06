ca3e0483ee5f48f444f6cecc9ccac51ad404bb0d
athenaz14 <athenaz11@nycstudents.net>
2026-05-06 09:20:22 -0400

    Update game.py

--- game.py
+++ game.py
@@ -3,9 +3,27 @@ class Sever:
         self.name = name
         self.money = money
         self.inventory = inventory
+
+    def display_info(self):
+        return f"Student: {self.name}, Email: {self.email}, Student ID: {self.student_id}"
+
 user_name = input("Hello! Please enter your name:")
 print(f"Welcome to {user_name}'s Resturaunt!")
 
 name = Sever(user_name,100, [])
 print(name.__dict__)
 
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
+name = BankAccount(user_name, 100)
+user_name.show_balance
+
+class CustomerHappiness(customer, name, happiness_level)-
