3ff3ec427e0b2b33f739ac3f8b084f2533629529
callac2 <callac2@nycstudents.net>
2026-05-07 09:26:21 -0400

    Update customers.py

--- customers.py
+++ customers.py
@@ -28,9 +28,38 @@ customer_orders_day_one = [
     "status": "LIVID."
 },
     ]
-for customer in customer_orders_day_one:
-    print(f"Customer Name: {customer["name"]}, Customer Status: {customer["status"]}")
+""" 
+customer_choice = input("Please enter the name of the customer you would like to serve.")
 
-    customer_choice = input("Please enter the name of the customer you would like to serve.")
-    if customer_choice in customer_orders_day_one:
-        print(customer_orders_day_one['name'],['price'],['food type'],['instructions'],['status'])
+for customer in customer_orders_day_one:
+    if customer_choice == customer_orders_day_one['name']:
+        print(f"Customer Name: {customer["name"]}, Customer Status: {customer["status"]}")
+
+   
+    if customer_choice in customer_orders_day_one: """
+"""         print(customer_orders_day_one['name'],['price'],['food type'],['instructions'],['status']) """
+
+earnings = []
+total_money = 0
+customer_history = []
+work = True
+for index, customer in enumerate(customer_orders_day_one):
+        print(index, ":", customer["name"],customer["status"])
+
+while work:
+    serve_customers = int(input("Serve customer? Type the number of the customer you want to serve first based on status."))
+    customer_history.append(customer_orders_day_one[serve_customers]["name"])
+    earnings.append(customer_orders_day_one[serve_customers]["price"])
+    work_continiue = input("continue working or calculate earnings and score for today? Enter Yes (continiue) or No (calculate)")
+
+    if work_continiue == "No":
+         work = False
+
+for customer in customer_history:
+    print(f"{customer_history}")
+
+print(earnings)
+for earning in earnings:
+    earnings += total_money
+
+print (f"{customer_history, total_money}")
