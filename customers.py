d3b5449cc8c5fa2a3548ba2a3915fd3fc310c8f2
callac2 <callac2@nycstudents.net>
2026-05-12 09:26:21 -0400

    Update customers.py

--- customers.py
+++ customers.py
@@ -4,6 +4,7 @@ customer_orders_day_one = [
 {
     "name": "Julia",
     "price": 12.99,
+    "tip": 2.00,
     "order": "waffles with syrup and blueberries",
     "instructions": "Toast the waffles, add syrup, add blueberries.",
     "status": "angry"
@@ -11,6 +12,7 @@ customer_orders_day_one = [
 {
     "name": "Gerald",
     "price": 4.99,
+    "tip": 0.50,
     "order": "hot coffee with milk and sugar",
     "instructions": "pour the coffee, add a splash of milk, and a teaspoon of sugar.",
     "status": "calm"
@@ -18,6 +20,7 @@ customer_orders_day_one = [
 {
     "name": "Agamemnon",
     "price": 120,
+    "tip": 0.000000000001,
     "order": "Two kids meal pancakes, an adult size bacon and eggs, covered in gold leaf.",
     "instructions": "Toast the waffles, add syrup, add blueberries, put on two plates. Fry the bacon and eggs, put on a plate.",
     "status": "LIVID."
@@ -25,6 +28,7 @@ customer_orders_day_one = [
 {
     "name": "Lyla",
     "price": 24.99,
+    "tip": 5.00,
     "order": "Mac and Cheese (3 orders plus extra cheese on one of them).",
     "instructions": "Heat up the mac anc cheese, plate, sprinke extra cheese on one of them.",
     "status": "LIVID."
@@ -32,6 +36,7 @@ customer_orders_day_one = [
 {
     "name": "",
     "price": 24.99,
+    "tip": 2.00,
     "order": "Mac and Cheese (3 orders plus extra cheese on one of them).",
     "instructions": "Heat up the mac anc cheese, plate, sprinke extra cheese on one of them.",
     "status": "LIVID."
@@ -39,6 +44,7 @@ customer_orders_day_one = [
 {
     "name": "Jake",
     "price": 12.99,
+    "tip": 4.00,
     "order": "Mac and Cheese (3 orders plus extra cheese on one of them).",
     "instructions": "Heat up the mac anc cheese, plate, sprinke extra cheese on one of them.",
     "status": "LIVID."
@@ -46,6 +52,7 @@ customer_orders_day_one = [
 {
     "name": "Marty",
     "price": 7.99,
+    "tip": 2.00,
     "order": "Mac and Cheese (3 orders plus extra cheese on one of them).",
     "instructions": "Heat up the mac anc cheese, plate, sprinke extra cheese on one of them.",
     "status": "LIVID."
@@ -53,21 +60,24 @@ customer_orders_day_one = [
 {
     "name": "Mr. Whalen",
     "price": 5000000.99,
+    "tip": 0.00,
     "order": "everyting. give me everything.",
     "instructions": "Heat up the mac anc cheese, plate, sprinke extra cheese on one of them.",
     "status": "LIVID."
 },
     ]
-class Customer:
+""" class Customer:
     def __init__(self, name, order, status):
         self.name = name 
         self.order = order
         self.status = status
-name = Customer() 
+name = Customer()  """
 
 
 earnings = []
+extra_tips = []
 total_money = 0
+totaltips = 0
 customer_history = []
 work = True
 for index, customer in enumerate(customer_orders_day_one):
@@ -77,16 +87,18 @@ while work:
     serve_customers = int(input("Serve customer? Type the number of the customer you want to serve first based on status."))
     customer_history.append(customer_orders_day_one[serve_customers]["name"])
     earnings.append(customer_orders_day_one[serve_customers]["price"])
+    extra_tips.append(customer_orders_day_one[serve_customers]["tip"])
     work_continiue = input("continue working or calculate earnings and score for today? Enter Yes (continiue) or No (calculate)")
 
     if work_continiue == "No":
          work = False
 
-if work == False:
+for earning in earnings:
+    total_money += earning
+for tips in extra_tips:
+            totaltips += extra_tips
 
-    for earning in earnings:
-        total_money += earning
+print(totaltips)
 print(total_money)
 print(f"{customer_history}")
     
-
