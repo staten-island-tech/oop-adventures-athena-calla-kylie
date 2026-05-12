016e807a0edc672d3a2200d2bbff7bb9f4ed5528
athenaz14 <athenaz11@nycstudents.net>
2026-05-12 09:26:22 -0400

    Update game.py

--- game.py
+++ game.py
@@ -30,12 +30,13 @@ customer_orders_day_one = [
     ]
 for customer in customer_orders_day_one:
     print(f"Name: {customer["name"]}, Status: {customer["status"]}")
-for customer in customer_orders_day_one:
+""" for customer in customer_orders_day_one:
     choice = input("Choose a customer to serve")
     if choice == customer["name"]:
         print(customer["name"])
     else:
-        print("No costumer found")
+        print("No costumer found") """
+"""
 class Sever:
     def __init__(self, name, money, inventory):
         self.name = name
@@ -59,64 +60,17 @@ class BankAccount:
     def show_balance(self):
         print(f"{self.owner} has ${self.__balance}")
 name = BankAccount(user_name, 100) 
+""" 
 class CustomerHappiness:
-    def __init__(self, name, happiness_level):
+    def __init__(self, name, status, happiness_level):
         self.name = name
         self.happiness_level = happiness_level
+        self.status = status
     def add_or_subtract(self, amount):
-        self.__balance += amount
-customer["name"] = CustomerHappiness(customer["name"], 10)
-customer["name"].add_or_subtract(-10)
-print(customer["name"].__dict__)
-
-
+        self.happiness_level += amount
+name = CustomerHappiness(customer["name"],customer["status"],10)
+print(name.__dict__)
+while customer["name"] not in 
+"""
         
 """
- calm = False  
-angry = False
-LIVID = False
-
-if happiness_level > 60:
-    calm = True
-elif happiness_level > 40:
-    angry = True
-
-
- """""" customer_orders_day_one = [
-{
-    "name": "Julia",
-    "price": 12.99,
-    "food type": "waffles with syrup and blueberries",
-    "instructions": "Toast the waffles, add syrup, add blueberries.",
-    "status": "angry"
-},
-{
-    "name": "Gerald",
-    "price": 4.99,
-    "food type": "hot coffee with milk and sugar",
-    "instructions": "pour the coffee, add a splash of milk, and a teaspoon of sugar.",
-    "status": "calm"
-},
-{
-    "name": "Agamemnon",
-    "price": 120,
-    "food type": "Two kids meal pancakes, an adult size bacon and eggs, covered in gold leaf.",
-    "instructions": "Toast the waffles, add syrup, add blueberries, put on two plates. Fry the bacon and eggs, put on a plate.",
-    "status": "LIVID."
-},
-{
-    "name": "Lyla",
-    "price": 24.99,
-    "food type": "Mac and Cheese (3 orders plus extra cheese on one of them).",
-    "instructions": "Heat up the mac anc cheese, plate, sprinke extra cheese on one of them.",
-    "status": "LIVID."
-},
-    ]
-for customer in customer_orders_day_one:
-    print(f"Name: {customer["name"]}, Status: {customer["status"]}")
-for customer in customer_orders_day_one:
-    choice = input("Choose a customer to serve")
-    if choice == customer["name"]:
-        print(customer["name"])
-    else:
-        print("No costumer found") """
