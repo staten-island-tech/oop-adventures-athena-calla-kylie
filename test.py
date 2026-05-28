6217d46e5abf33d95016856b689c31ce558af546
callac2 <callac2@nycstudents.net>
2026-05-28 08:49:37 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,55 @@
 import random
 import time
+
+class server_profile:
+        def __init__(self, name, gender, workdays):
+            self.name = name 
+            self.gender = gender
+            self.workdays = workdays
+class Server:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+user_name = input("Hello! Please enter your name:")
+print(f"Welcome to {user_name}'s Resturaunt! You are now a server at rank {1} with {"ZERO"} customers served so far.")
+print("If you need instructions on how to play, write 'Yes'. If you are ready to play now, write 'No' in the box below.")
+
+instructions = input("Instructions? ::")
+
+if instructions == ("Yes"):
+        print(f"Hello my dear {user_name}!! I do hope your day is going well. Are you ready to master the restauraunt business? Or maybe not... Maybe you aren't ready. Are you?")
+        ready = input("Are you ready for this?")
+        print(f"Oh okay good, good. I am Luigi, the god of the restauraunt business. I will show you the ways of serving. Ready?")
+        print("Step one: Show the customers the menu. Make sure they choose an item, which will print below for you.")
+        ready2 = input("Got it??")
+        print("Step two: check the customer's status (also printed below). If they are anything below annoyed, you need to REALLY make sure to get their order right... or else you lose money.")
+        ready3 = input("Understood?")
+        print(f"well then, {user_name}, it seems you are well prepared for the ordeal ahead. try to collect as many tips and earnings as you can, you can use them in the inventory store when you finish day one. Best of luck {user_name}, and I'll see you on the other side! XX, Luigi")
+        finalready = input("Ready?")
+        print("GAME START!!")
+else: print("GAME START!!")
+
+name = Server(user_name, 0, ["nothing yet"])
+print(name.__dict__)
+
+serve = input("Serve your first customer by entering ['Next Customer!']")
+## REMEMBER TO INSERT ATHENA AND KYLIE AND MY CODE HERE
+
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
+name = BankAccount(user_name, 100) 
+
 class menus():
     def __init__(self, order_name, description, price,):
         self.order_name = order_name
